6e69ebe5c97ffd285bdc181488e14246e9c21527
Oliver Stanton <os349@student.aru.ac.uk>
2024-03-01 07:06:23 +0000

    Moved assets folder, no longer hard paths
    This folder will need to be copied to the .exe location for that to work properly, but oh well. Every external asset should be in it so it should be simple as copy paste and not a long slog of one by one copy pasting assets kinda thing

--- src/Level/TailsEntity.cpp
+++ src/Level/TailsEntity.cpp
@@ -9,20 +9,19 @@ void tails::TailsEntity::spawn()
 {
     Entity::spawn();
 
-    // Use full path because local path don't work for some reason, work out?!
-    getResourceManager().loadTexture("tails", "F:/Libraries/TailsEngine/src/Assets/Textures/Tails.png");
+    getResourceManager().loadTexture("tails", "Assets/Textures/Tails.png");
     setTexture(getResourceManager().textureManager.getAssetRef("tails"));
     setOrigin(32.f, 32.f);
     setPosition(480.f, 320.f);
 
     currentSpeed = baseSpeed * walkSpeedMultiplier;
 
-    getResourceManager().loadMusic("F:/Libraries/TailsEngine/src/Assets/Music/TailsLab.ogg");
+    getResourceManager().loadMusic("Assets/Music/TailsLab.ogg");
     getResourceManager().music.setVolume(50.f);
     getResourceManager().music.play();
 
     // Load a sound for use on user input
-    getResourceManager().loadSound("audio_jungle", "F:/Libraries/TailsEngine/src/Assets/Sounds/AudioJungle.ogg");
+    getResourceManager().loadSound("audio_jungle", "Assets/Sounds/AudioJungle.ogg");
     sound.setBuffer(getResourceManager().soundManager.getAssetRef("audio_jungle"));
 }
 
@@ -59,7 +58,7 @@ void tails::TailsEntity::update(float deltaTime)
         {
             setOtherMusic = true;
             // play some other music
-            getResourceManager().loadAndPlayMusic("F:/Libraries/TailsEngine/src/Assets/Music/EmeraldCity.ogg");
+            getResourceManager().loadAndPlayMusic("Assets/Music/EmeraldCity.ogg");
         }
     }
 }
