e81f0f6e6c0b25cd0ed85432b6cdb9a747cd8e96
Oliver Stanton <os349@student.aru.ac.uk>
2024-03-03 04:52:16 +0000

    main.cpp comment

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,8 @@
 #include <TailsEngine/Core/ApplicationWindow.h>
 
 /**
- * Idea - levels will be stored in .json or something similar. Entities will be read within the game from it,
- * creating an array that creates Entity objects. PlayerStart or something similar will be the player start point.
- * Having these entities create on their own is essential, because the idea is to not have to make the developer
- * create their own classes to manually spawn things in code. Obviously things need to spawn in code, for dynamic
- * entities, but you get what I mean.
- *
- * Tiled maps or LDTK or whatever for making the levels. It'll kind of be like Valve's .vmf files, but just for
- * this engine.
+ * This displays the most bare bones code to use the engine. Default level JSON will be declared in some kind of
+ * .ini file or something the engine reads on startup
  */
 
 int main()
