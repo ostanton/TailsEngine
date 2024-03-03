43446d2783728bb9bf2b4ccc383e3005cd16509c
Oliver Stanton <os349@student.aru.ac.uk>
2024-03-03 04:48:44 +0000

    More Debug::log() overrides

--- include/TailsEngine/Debug/Debug.h
+++ include/TailsEngine/Debug/Debug.h
@@ -1,12 +1,19 @@
 ﻿#pragma once
 #include <string>
 
+namespace sf
+{
+class String;
+}
+
 namespace tails
 {
 class Debug
 {
 public:
     static void log(const std::string& string);
+    static void log(const char* string);
+    static void log(const sf::String& string);
 };
 
 }
--- src/Debug/Debug.cpp
+++ src/Debug/Debug.cpp
@@ -1,8 +1,19 @@
 ﻿#include "TailsEngine/Debug/Debug.h"
 
 #include <iostream>
+#include <SFML/System/String.hpp>
 
 void tails::Debug::log(const std::string& string)
 {
     std::cout << string << "\n";
+}
+
+void tails::Debug::log(const char* string)
+{
+    std::cout << string << "\n";
+}
+
+void tails::Debug::log(const sf::String& string)
+{
+    log(string.toAnsiString());
 }
