94eb3a6e5dd4d61471d48d4c94888d2d7eb5f114
Oliver Stanton <os349@student.aru.ac.uk>
2024-03-03 22:29:50 +0000

    Start of AnimationPlayer class for sprite animations

---
+++ include/TailsEngine/Core/AnimationPlayer.h
@@ -0,0 +1,77 @@
+﻿#pragma once
+#include <string>
+#include <unordered_map>
+#include <SFML/Graphics/Rect.hpp>
+#include <SFML/System/Vector2.hpp>
+
+namespace sf
+{
+class Sprite;
+class Texture;
+}
+
+namespace tails
+{
+struct FrameInfo
+{
+    sf::IntRect cell;
+};
+
+struct AnimationInfo
+{
+    std::vector<FrameInfo> frames;
+    /**
+     * \brief The sprite sheet where the animation is located. This comes from an asset cache, so its memory
+     * is managed there
+     */
+    sf::Texture* spriteSheet;
+    sf::Vector2u cellSize;
+    sf::Vector2u startingCellPosition;
+    bool playing {false};
+    size_t currentFrame {0};
+    bool loop {true};
+
+    AnimationInfo(
+        sf::Texture* inSpriteSheet,
+        const sf::Vector2i inCellSize,
+        const sf::Vector2u inStartingCellPosition)
+        :
+    spriteSheet(inSpriteSheet),
+    cellSize(inCellSize),
+    startingCellPosition(inStartingCellPosition) {}
+
+    void setupFrames();
+};
+
+class AnimationPlayer
+{
+public:
+    void update(float deltaTime);
+
+    void setTargetSprite(sf::Sprite* sprite);
+    void addAnimation(const std::string& name, sf::Texture* spriteSheet, const sf::Vector2i cellSize,
+        const sf::Vector2u startingCellPosition = {0, 0});
+    void addAnimation(const std::string& name, AnimationInfo& animation);
+    void removeAnimation(const std::string& name);
+
+    void playAnimation(const std::string& name, bool playFromStart = false);
+    void pauseAnimation();
+    void pauseAnimation(const std::string& name);
+    void stopAllAnimations();
+    
+    bool isAnimationPlaying();
+    std::string& getCurrentAnimation();
+
+private:
+    std::unordered_map<std::string, AnimationInfo> m_animations;
+    /**
+     * \brief Sprite to animate. This comes from the Entity that has the sprite component itself, so its memory
+     * is managed there
+     */
+    sf::Sprite* m_sprite {nullptr};
+    std::string m_currentAnimation;
+    float m_playRate {1.f};
+    float m_animTimer {0.f};
+};
+
+}
---
+++ src/Core/AnimationPlayer.cpp
@@ -0,0 +1,155 @@
+﻿#include "TailsEngine/Core/AnimationPlayer.h"
+
+#include <ranges>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+
+#include "TailsEngine/Debug/Debug.h"
+
+void tails::AnimationInfo::setupFrames()
+{
+    // Create the frames of the animation from the sprite sheet and cell size
+    const sf::Vector2u cellsInSheet {
+        spriteSheet->getSize().x / cellSize.x,
+        spriteSheet->getSize().y / cellSize.y
+    };
+
+    for (unsigned i {0}; i < cellsInSheet.x; i++)
+    {
+        FrameInfo frameInfo;
+        // Set cell size. If i != 0 (>), we add our cell size and multiple by i to get the left position
+        const sf::IntRect frameCell {
+            static_cast<int>(i == 0 ? startingCellPosition.x : (startingCellPosition.x + cellSize.x) * i),
+            static_cast<int>(startingCellPosition.y),
+            static_cast<int>(cellSize.x),
+            static_cast<int>(cellSize.y)
+        };
+        frameInfo.cell = frameCell;
+        frames.push_back(frameInfo);
+    }
+}
+
+void tails::AnimationPlayer::update(float deltaTime)
+{
+    if (!m_sprite)
+    {
+        Debug::log("AnimationPlayer sprite is not set");
+        return;
+    }
+    
+    if (!m_animations.contains(m_currentAnimation))
+        return;
+
+    AnimationInfo& animation = m_animations.at(m_currentAnimation);
+    
+    if (!animation.playing)
+        return;
+
+    m_sprite->setTextureRect(animation.frames[animation.currentFrame].cell);
+
+    m_animTimer += deltaTime * m_playRate;
+
+    // Return if we haven't reached the next frame yet
+    if (m_animTimer < 1.f / m_playRate)
+        return;
+    
+    animation.currentFrame++;
+
+    if (animation.currentFrame >= animation.frames.size())
+    {
+        if (animation.loop)
+        {
+            animation.currentFrame = 0;
+        }
+        else
+        {
+            animation.playing = false;
+        }
+    }
+
+    m_animTimer = 0.f;
+}
+
+void tails::AnimationPlayer::setTargetSprite(sf::Sprite* sprite)
+{
+    m_sprite = sprite;
+}
+
+void tails::AnimationPlayer::addAnimation(const std::string& name, sf::Texture* spriteSheet,
+    const sf::Vector2i cellSize, const sf::Vector2u startingCellPosition)
+{
+    AnimationInfo animationInfo(spriteSheet, cellSize, startingCellPosition);
+    addAnimation(name, animationInfo);
+}
+
+void tails::AnimationPlayer::addAnimation(const std::string& name, AnimationInfo& animation)
+{
+    if (m_animations.contains(name))
+        return;
+    
+    animation.setupFrames();
+    m_animations.emplace(name, animation);
+}
+
+void tails::AnimationPlayer::removeAnimation(const std::string& name)
+{
+    if (!m_animations.contains(name))
+        return;
+
+    m_animations.erase(name);
+}
+
+void tails::AnimationPlayer::playAnimation(const std::string& name, bool playFromStart)
+{
+    if (!m_animations.contains(name))
+        return;
+
+    m_currentAnimation = name;
+    m_animations.at(name).playing = true;
+
+    if (m_sprite->getTexture() != m_animations.at(name).spriteSheet)
+        m_sprite->setTexture(*m_animations.at(name).spriteSheet);
+
+    if (playFromStart)
+        m_animations.at(name).currentFrame = 0;
+}
+
+void tails::AnimationPlayer::pauseAnimation()
+{
+    pauseAnimation(m_currentAnimation);
+}
+
+void tails::AnimationPlayer::pauseAnimation(const std::string& name)
+{
+    if (!m_animations.contains(name))
+        return;
+
+    m_animations.at(name).playing = false;
+}
+
+void tails::AnimationPlayer::stopAllAnimations()
+{
+    m_currentAnimation = "";
+
+    for (auto& animation : std::ranges::views::values(m_animations))
+    {
+        animation.playing = false;
+        animation.currentFrame = 0;
+    }
+}
+
+bool tails::AnimationPlayer::isAnimationPlaying()
+{
+    for (const auto& animation : std::ranges::views::values(m_animations))
+    {
+        if (animation.playing)
+            return true;
+    }
+
+    return false;
+}
+
+std::string& tails::AnimationPlayer::getCurrentAnimation()
+{
+    return m_currentAnimation;
+}
