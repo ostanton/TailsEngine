617099a41715edecdae020206f6d7f40fc3446d7
ostanton <114495956+ostanton@users.noreply.github.com>
2024-06-12 08:17:32 +0100

    Forgot the Config.hpp file in the last push somehow?? Either way take it!

---
+++ include/Tails/Config.hpp
@@ -0,0 +1,18 @@
+#ifndef TAILS_CONFIG_HPP
+#define TAILS_CONFIG_HPP
+
+#include <SFML/Config.hpp>
+
+// tails current version numbers
+#define TAILS_VERSION_MAJOR 0
+#define TAILS_VERSION_MINOR 0
+#define TAILS_VERSION_PATCH 0
+
+// tails export/import macros
+#define TAILS_API_EXPORT SFML_API_EXPORT
+#define TAILS_API_IMPORT SFML_API_IMPORT
+
+// general tails api macro
+#define TAILS_API
+
+#endif // TAILS_CONFIG_HPP
